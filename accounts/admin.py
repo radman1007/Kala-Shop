c73432f087c51fb6bc151564534e7b151b4ecaea
radman1007 <radman.cod110@gmail.com>
2024-07-04 18:02:45 +0330

    debug

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     add_form = CustomUserCreationForm
