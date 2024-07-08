636ee25ebfb21017a55575adab69f337d2c3875b
radman1007 <radman.cod110@gmail.com>
2024-07-08 22:49:51 +0330

    test

--- config/settings.py
+++ config/settings.py
@@ -105,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKEND = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
