06a904e458dc9865bc9d34a174679ca51cda82f8
radman1007 <radman.cod110@gmail.com>
2024-07-08 22:43:34 +0330

    start all auth

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
     'accounts',
     'pages',
 ]
