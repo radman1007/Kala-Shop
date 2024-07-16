327eda8fe242fe450ae749146b2299b8a6d9af33
radman1007 <radman.cod110@gmail.com>
2024-07-16 16:37:27 +0330

    debug

--- products/views.py
+++ products/views.py
@@ -3,6 +3,6 @@ from django.views.generic import ListView, DetailView
 from .models import Product
 
 class ProductListView(ListView):
-    model = Product
+    queryset = Product.objects.filter(active=True)
     template_name = 'products/product_list.html'
     context_object_name = 'products'
