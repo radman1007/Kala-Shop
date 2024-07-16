cb2d64a96ba4843642abfb64c8a9cccc4a9e469e
radman1007 <radman.cod110@gmail.com>
2024-07-16 16:40:26 +0330

    debug and add detail

--- products/views.py
+++ products/views.py
@@ -6,3 +6,9 @@ class ProductListView(ListView):
     queryset = Product.objects.filter(active=True)
     template_name = 'products/product_list.html'
     context_object_name = 'products'
+    
+    
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
