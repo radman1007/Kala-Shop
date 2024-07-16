e32eef6401a330a77fd8a7da7552f27731320bde
radman1007 <radman.cod110@gmail.com>
2024-07-17 01:48:01 +0330

    test

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
+from .forms import CommentForm
 
 class ProductListView(ListView):
     queryset = Product.objects.filter(active=True)
@@ -12,3 +14,8 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm
+        return context
