9580f1de729e039f6af565e089287bf5f1b068b2
radman1007 <radman.cod110@gmail.com>
2024-07-16 16:47:20 +0330

    add get url

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -10,3 +11,8 @@ class Product(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+    def get_absolute_url(self):
+        return reverse("product_detail", args={self.pk})
+    
