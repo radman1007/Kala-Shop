68d24e40e07e7b56d33b6b64b5e808ca57b5b749
radman1007 <radman.cod110@gmail.com>
2024-07-16 19:06:13 +0330

    debug

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('pages/', include('pages.urls')),
+    path('', include('products.urls')),
     path('accounts/', include('allauth.urls')),
-    path('products/', include('products.urls')),
 ]
