50bce676beb0ca26b1c0659bb4d4791d5139e51b
radman1007 <radman.cod110@gmail.com>
2024-07-16 16:25:30 +0330

    add name

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProductListView
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name=''),
+    path('', ProductListView.as_view(), name='product_list'),
 ]
